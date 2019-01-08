18848093a01b066bbb09ffd2747aaaf6586ee878
princeahmed <israilahmed5@gmail.com>
2019-01-08 10:45:54 +0600

    add-generator-rule menu added

--- includes/admin/class-admin-menu.php
+++ includes/admin/class-admin-menu.php
@@ -3,4 +3,37 @@
 namespace Pluginever\WCSerialNumberPro\Admin;
 class Admin_Menu{
 
+	/**
+	 * Admin_Menu constructor.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+
+	function __construct()
+	{
+		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+	}
+
+	function admin_menu(){
+		add_submenu_page( 'serial-numbers', __( 'Add generator rule', 'wc-serial-numbers' ), __( 'Add generator rule', 'wc-serial-numbers' ), 'manage_woocommerce', 'add-generator-rule', array(
+			$this, 'add_generator_rule' ) );
+	}
+
+	/**
+	 * Get the Add generator rule template
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+
+	function add_generator_rule(){
+		wsn_get_template_part('add-serial-number-rule.php', true);
+	}
+
+
+
+
 }
--- templates/generate-serial-number.php
+++ templates/generate-serial-number.php
@@ -1,3 +1,60 @@
-<h3 class="wp-heading-inline"><?php _e('Automatic License Key Generator Rules', 'wc-serial-numbers') ?></h3>
+<?php
 
+$row_action = empty($_REQUEST['row_action']) ? '' : $_REQUEST['row_action'];
+
+$is_product_tab = !empty(get_query_var('is_product_tab')) ? get_query_var('is_product_tab') : '';
+
+if (!$row_action) {
+
+	include WPWSNP_INCLUDES . '/admin/generate-serial-table.php';
+
+	$rule_list = new Pluginever\WCSerialNumberPro\Admin\Generate_Serial_Table($is_product_tab);
+
+	$rule_list->prepare_items();
+
+	?>
+
+	<div class="wrap wsn-container">
+		<?php if (!$is_product_tab) { ?>
+
+			<h1 class="wp-heading-inline"><?php _e('Generator Rules', 'wc-serial-numbers') ?></h1>
+
+			<a href="<?php echo WPWSN_ADD_GENERATE_RULE ?>" class="page-title-action"><?php _e('Add new generator rules', 'wc-serial-numbers') ?></a>
+
+
+		<?php } ?>
+
+		<div class="wsn-body">
+			<?php
+			if (!$is_product_tab) {
+				//$rule_list->search_box('Search', 'search_id');
+				echo '<form action="" method="GET">';
+				echo $rule_list->search_box(__('Search'), 'wsn_generate_serial');
+				echo '<input type="hidden" name="page" value="' . esc_attr($_REQUEST['page']) . '"/></form>'; // form end
+			}
+
+			echo '<form id="wsn-serial-numbers-table" action="' . admin_url('admin-post.php') . '" method="post">
+			  	 <input type="hidden" name="wsn-serial-numbers-table-action">'
+				. wp_nonce_field('wsn-serial-numbers-table', 'wsn-serial-numbers-table-nonce');
+
+			$rule_list->display();
+
+			echo '</form>';
+
+			?>
+		</div>
+
+	</div>
+
+<?php } elseif ($row_action == 'edit') {
+
+	include WPWSN_TEMPLATES_DIR . '/add-serial-number-page.php';
+
+} elseif ($row_action == 'delete') {
+
+	wp_delete_post($_REQUEST['serial_number']);
+
+	wp_redirect(admin_url('admin.php?page=serial-numbers'));
+
+} ?>
 
