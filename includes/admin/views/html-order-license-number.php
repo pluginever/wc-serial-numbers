3f67a182ad2a66e1293cca7abb4de3712ca77243
princeahmed <israilahmed5@gmail.com>
2019-03-04 14:14:38 +0600

    order expire date added

--- includes/admin/views/html-order-license-number.php
+++ includes/admin/views/html-order-license-number.php
@@ -4,7 +4,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 global $post, $wpdb;
+
 $serial_numbers = wcsn_get_serial_numbers( [ 'order_id' => $post->ID ] );
+
 if ( sizeof( $serial_numbers ) > 0 ) { ?>
 	<div class="wcsn-metabox-table-wrapper">
 		<table class="wcsn-metabox-table wp-list-table widefat fixed striped" cellspacing="0">
@@ -13,7 +15,7 @@ if ( sizeof( $serial_numbers ) > 0 ) { ?>
 				<th><?php _e( 'Product', 'wc-serial-numbers' ) ?></th>
 				<th><?php _e( 'Serial Key', 'wc-serial-numbers' ) ?></th>
 				<th><?php _e( 'Activation Limit', 'wc-serial-numbers' ) ?></th>
-				<th><?php _e( 'Validity', 'wc-serial-numbers' ) ?></th>
+				<th><?php _e( 'Expire Date', 'wc-serial-numbers' ) ?></th>
 				<th><?php _e( 'Status', 'wc-serial-numbers' ) ?></th>
 				<th><?php _e( 'Actions', 'wc-serial-numbers' ) ?></th>
 			</tr>
@@ -22,10 +24,14 @@ if ( sizeof( $serial_numbers ) > 0 ) { ?>
 			<?php $i = 1;
 			foreach ( $serial_numbers as $serial_number ): $i ++ ?>
 				<tr class="<?php echo ( $i % 2 == 1 ) ? 'alternate' : ''; ?>">
-					<td><a href="<?php echo get_edit_post_link( $serial_number->product_id ); ?>"><?php echo get_the_title( $serial_number->product_id ); ?></a></td>
+					<td>
+						<a href="<?php echo get_edit_post_link( $serial_number->product_id ); ?>"><?php echo get_the_title( $serial_number->product_id ); ?></a>
+					</td>
 					<td><span style="max-width: 150px;"><?php echo $serial_number->serial_key; ?></span></td>
 					<td><?php echo ( $serial_number->activation_limit ) ? $serial_number->activation_limit : __( 'N/A', 'wc-serial-numbers' ); ?></td>
-					<td><?php echo ! empty( $serial_number->validity ) ? sprintf( _n( '%s Day', '%s Days', $serial_number->validity, 'wc-serial-numbers' ), number_format_i18n( $serial_number->validity ) ) : __( 'Never expire', 'wc-serial-numbers' ); ?></td>
+					<td><?php
+						$order_date = $serial_number->order_date;
+						echo ! empty( $serial_number->validity ) ? date( 'Y-m-d', strtotime( $order_date . ' + ' . $serial_number->validity . ' Day ' ) ) : __( 'Never expire', 'wc-serial-numbers' ); ?></td>
 					<td><?php echo ! empty( $serial_number->status ) ? wcsn_get_serial_statuses()[ $serial_number->status ] : '&#45;'; ?></td>
 					<td>
 						<a href="<?php echo add_query_arg( array(
@@ -47,18 +53,18 @@ if ( sizeof( $serial_numbers ) > 0 ) { ?>
 	<?php
 }
 ?>
-<style>
-	.wcsn-metabox-table th {
-		text-align: left;
-		padding: 1em;
-		font-weight: 600;
-		color: #333;
-		background: #efefef;
-		-webkit-touch-callout: none;
-		-webkit-user-select: none;
-		-moz-user-select: none;
-		-ms-user-select: none;
-		user-select: none;
-	}
-</style>
+	<style>
+		.wcsn-metabox-table th {
+			text-align: left;
+			padding: 1em;
+			font-weight: 600;
+			color: #333;
+			background: #efefef;
+			-webkit-touch-callout: none;
+			-webkit-user-select: none;
+			-moz-user-select: none;
+			-ms-user-select: none;
+			user-select: none;
+		}
+	</style>
 <?php
