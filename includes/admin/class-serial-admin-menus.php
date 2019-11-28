2c184c4c05e2ab8478cb58b2c86f61a08790f99a
manzurahammed <manzur.ahammed236@gmail.com>
2019-11-28 15:45:13 +0600

    add serial number page

--- includes/admin/class-serial-admin-menus.php
+++ includes/admin/class-serial-admin-menus.php
@@ -14,15 +14,27 @@ class WC_Serial_Numbers_Admin_Menus {
 	 * Adds page to admin menu
 	 */
 	function admin_menu() {
-		add_menu_page( __( 'WC Serial Numbers', 'wc-serial-numbers' ), __( 'Serial Numbers', 'wc-serial-numbers' ), 'manage_woocommerce', 'wc-serial-numbers', array( $this, 'serial_numbers_page' ), 'dashicons-admin-network', '55.9' );
-		add_submenu_page( 'wc-serial-numbers', __( 'Serial Numbers', 'wc-serial-numbers' ), __( 'Serial Numbers', 'wc-serial-numbers' ), 'manage_woocommerce', 'wc-serial-numbers', array( $this, 'serial_numbers_page' ) );
-		add_submenu_page( 'wc-serial-numbers', __( 'API Doc', 'wc-serial-numbers' ), __( 'API Doc', 'wc-serial-numbers' ), 'manage_woocommerce', 'wcsn-api-doc', array( $this, 'api_doc_page' ) );
+		add_menu_page( __( 'WC Serial Numbers', 'wc-serial-numbers' ), __( 'Serial Numbers', 'wc-serial-numbers' ), 'manage_woocommerce', 'wc-serial-numbers', array(
+			$this,
+			'serial_numbers_page'
+		), 'dashicons-admin-network', '55.9' );
+		add_submenu_page( 'wc-serial-numbers', __( 'Serial Numbers', 'wc-serial-numbers' ), __( 'Serial Numbers', 'wc-serial-numbers' ), 'manage_woocommerce', 'wc-serial-numbers', array(
+			$this,
+			'serial_numbers_page'
+		) );
+		add_submenu_page( 'wc-serial-numbers', __( 'API Doc', 'wc-serial-numbers' ), __( 'API Doc', 'wc-serial-numbers' ), 'manage_woocommerce', 'wcsn-api-doc', array(
+			$this,
+			'api_doc_page'
+		) );
 	}
 
-	public function serial_numbers_page(){
-		serial_numbers_get_views('serial-number-page.php');
+	public function serial_numbers_page() {
+		serial_numbers_get_views( 'serial-number-page.php' );
 	}
 
+	public function api_doc_page() {
+		serial_numbers_get_views( 'api-doc-page.php' );
+	}
 
 
 }
