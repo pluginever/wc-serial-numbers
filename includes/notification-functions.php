d97fc78a6033586d05cafe6a5bfac7f15aa9800f
MD Sultan Nasir Uddin <manikdrmc@gmail.com>
2019-12-10 18:30:43 +0600

    add notification

--- includes/notification-functions.php
+++ includes/notification-functions.php
@@ -69,3 +69,45 @@ function wc_serial_numbers_admin_bar_notification_styles() {
 
 add_action( 'admin_head', 'wc_serial_numbers_admin_bar_notification_styles' );
 add_action( 'wp_head', 'wc_serial_numbers_admin_bar_notification_styles' );
+
+
+/**
+ * @param int $stock
+ *
+ * @return array
+ * @since 1.0.0
+ */
+function wc_serial_numbers_get_low_stocked_products( $force = false, $stock = 10 ) {
+	$transient = md5( 'wcsn_low_stocked_products' . $stock );
+	if ( $force || false == $low_stocks = get_transient( $transient ) ) {
+		global $wpdb;
+		$product_ids   = $wpdb->get_results( "select post_id product_id, 0 as count from $wpdb->postmeta where meta_key='_is_serial_number' AND meta_value='yes'" );
+		$serial_counts = $wpdb->get_results( $wpdb->prepare( "SELECT product_id, count(id) as count FROM $wpdb->wcsn_serials_numbers where status='available' AND product_id IN (select post_id from $wpdb->postmeta where meta_key='_is_serial_number' AND meta_value='yes') 
+																group by product_id having count < %d order by count asc", $stock ) );
+		$serial_counts = wp_list_pluck( $serial_counts, 'count', 'product_id' );
+		$product_ids   = wp_list_pluck( $product_ids, 'count', 'product_id' );
+		$low_stocks    = array_replace( $product_ids, $serial_counts );
+		set_transient( $transient, $low_stocks, time() + 60 * 60 );
+	}
+
+	return $low_stocks;
+}
+
+function wc_serial_numbers_send_low_stock_notification() {
+	$notification = wc_serial_numbers_get_settings( 'low_stock_notification', false );
+	if ( ! $notification ) {
+		return false;
+	}
+	$stock_threshold = wc_serial_numbers_get_settings( 'low_stock_threshold', 10 );
+
+	$low_stocked_products = wc_serial_numbers_get_low_stocked_products( $stock_threshold );
+	if ( empty( $low_stocked_products ) ) {
+		return false;
+	}
+
+
+
+
+}
+
+add_action( 'wcsn_hourly_event', 'wc_serial_numbers_send_low_stock_notification' );
