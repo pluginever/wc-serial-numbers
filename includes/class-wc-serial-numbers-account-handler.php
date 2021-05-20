4acf559ca24111b396e903ab9b1224279967260e
sudipto <sudipto004.ruet@gmail.com>
2021-05-20 14:52:11 +0600

    order with no serial numbers options updated

--- includes/class-wc-serial-numbers-account-handler.php
+++ includes/class-wc-serial-numbers-account-handler.php
@@ -8,7 +8,7 @@ class WC_Serial_Numbers_Account_handler {
 	 * @since 1.2.8
 	 */
 	public function __construct() {
-		//register end points in the account page
+		// register end points in the account page
 		add_filter( 'woocommerce_get_query_vars', array( $this, 'wc_serial_numbers_query_vars' ) );
 		add_filter( 'woocommerce_endpoint_serial-numbers_title', array( $this, 'wc_serial_numbers_endpoint_title' ) );
 		add_filter( 'woocommerce_account_menu_items', array( $this, 'wc_serial_numbers_add_menu_items' ) );
@@ -48,12 +48,12 @@ class WC_Serial_Numbers_Account_handler {
 	 * @since 1.2.8
 	 */
 	public function wc_serial_numbers_add_menu_items( $menu_items ) {
-		//insert after account details
+		// insert after account details
 		$menu_item_key   = 'serial-numbers';
 		$menu_item_value = apply_filters( 'wc_serial_numbers_account_menu_title', __( 'Serial Numbers', 'wc-serial-numbers' ) );
 
 		$add_before_index = array_search( 'edit-account', array_keys( $menu_items ), true );
-		if ( false == $add_before_index ) {
+		if ( false === $add_before_index ) {
 			$menu_items[ $menu_item_key ] = $menu_item_value;
 		} else {
 			$add_before_index ++;
@@ -65,32 +65,36 @@ class WC_Serial_Numbers_Account_handler {
 
 	/**
 	 * Serial numbers end point content
+	 *
 	 * @since 1.2.8
 	 */
 	public function wc_serial_numbers_endpoint_content() {
 		wc_print_notices();
-		$current_user_id     = get_current_user_id();
-		$customer_orders     = get_posts( array(
-			'posts_per_page' => - 1,
-			'post_type'      => 'shop_order',
-			'post_status'    => 'wc-completed',
-			'meta_query'     => array(
-				array(
-					'key'     => '_customer_user',
-					'value'   => $current_user_id,
-					'compare' => '=',
+		$current_user_id = get_current_user_id();
+		$customer_orders = get_posts(
+			array(
+				'posts_per_page' => -1,
+				'post_type'      => 'shop_order',
+				'post_status'    => 'wc-completed',
+				'meta_query'     => array(
+					array(
+						'key'     => '_customer_user',
+						'value'   => $current_user_id,
+						'compare' => '=',
+					),
 				),
 			)
-		) );
+		);
+
 		$serial_numbers_data = array();
 		if ( is_array( $customer_orders ) && count( $customer_orders ) ) {
 			foreach ( $customer_orders as $order ) {
 				$single_order = wc_get_order( $order->ID );
 
-				//check if the order has attached serial numbers
+				// check if the order has attached serial numbers
 				$total_ordered_serial_numbers = wc_serial_numbers_order_has_serial_numbers( $single_order );
 				if ( empty( $total_ordered_serial_numbers ) ) {
-					return;
+					continue;
 				}
 				$serial_numbers = WC_Serial_Numbers_Query::init()->from( 'serial_numbers' )->where( 'order_id', $single_order->get_id() )->get();
 				if ( is_array( $serial_numbers ) && count( $serial_numbers ) ) {
@@ -108,30 +112,34 @@ class WC_Serial_Numbers_Account_handler {
 					}
 				}
 			}
-		} else {
-			?>
+		} else { ?>
 			<div class="woocommerce-message woocommerce-message--info woocommerce-Message woocommerce-Message--info woocommerce-info">
 				<a class="woocommerce-Button button" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>"><?php esc_html_e( 'Browse products', 'woocommerce' ); ?></a>
 				<?php esc_html_e( apply_filters( 'wc_serial_numbers_account_empty_message', __( 'No serial numbers has been added yet.', 'wc-serial-numbers' ) ) ); ?>
 			</div>
 			<?php
 		}
-
+		
 		if ( is_array( $serial_numbers_data ) && count( $serial_numbers_data ) ) {
-			$columns = apply_filters( 'wc_serial_numbers_account_table_columns', array(
-				'order'            => __( 'Order', 'wc-serial-numbers' ),
-				'serial_key'       => __( 'Serial Key', 'wc-serial-numbers' ),
-				'product'          => __( 'Product', 'wc-serial-numbers' ),
-				'activation_limit' => __( 'Activation Limit', 'wc-serial-numbers' ),
-				'expire_date'      => __( 'Expires', 'wc-serial-numbers' ),
-			) );
+			$columns = apply_filters(
+				'wc_serial_numbers_account_table_columns',
+				array(
+					'order'            => __( 'Order', 'wc-serial-numbers' ),
+					'serial_key'       => __( 'Serial Key', 'wc-serial-numbers' ),
+					'product'          => __( 'Product', 'wc-serial-numbers' ),
+					'activation_limit' => __( 'Activation Limit', 'wc-serial-numbers' ),
+					'expire_date'      => __( 'Expires', 'wc-serial-numbers' ),
+				)
+			);
 			?>
 			<table class="woocommerce-table woocommerce-table--order-serial_numbers shop_table shop_table_responsive order_details" cellspacing="0" cellpadding="6" border="1">
 			<thead>
 			<tr>
-				<?php foreach ( $columns as $key => $label ) {
+				<?php
+				foreach ( $columns as $key => $label ) {
 					echo sprintf( '<th class="td %s" scope="col" style="text-align:left;">%s</th>', sanitize_html_class( $key ), $label );
-				} ?>
+				}
+				?>
 			</tr>
 			</thead>
 			<tbody>
@@ -158,7 +166,7 @@ class WC_Serial_Numbers_Account_handler {
 								echo $serial_number['activation_limit'];
 							}
 							break;
-						case 'expire_date' :
+						case 'expire_date':
 							if ( empty( $serial_number['validity'] ) ) {
 								echo __( 'Lifetime', 'wc-serial-numbers' );
 							} else {
