7ceb99f4bbd9203014ef7f17393a40f2c48de86d
fhrabbi555 <fhrabbi555@gmail.com>
2024-08-29 16:07:54 +0600

    Fix #426

--- wc-serial-numbers.php
+++ wc-serial-numbers.php
@@ -7,7 +7,7 @@
  * Requires at least:    5.0
  * Requires PHP:         7.4
  * Author:               PluginEver
- * Author URI:           http://pluginever.com
+ * Author URI:           https://pluginever.com
  * License:              GPL v2 or later
  * License URI:          https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:          wc-serial-numbers
