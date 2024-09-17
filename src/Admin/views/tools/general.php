1ed8dfe86734f1ca4cadd8a47a6f9519c022efe8
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2024-09-17 16:10:38 +0600

    Update textdomain

--- src/Admin/views/tools/general.php
+++ src/Admin/views/tools/general.php
@@ -11,21 +11,21 @@ defined( 'ABSPATH' ) || exit;
 
 <div class="pev-card">
 	<div class="pev-card__header">
-		<h2 class="pev-card__title"><?php esc_html_e( 'Bulk Keys Generator', 'wc-serial-numbers-pro', 'wc-serial-numbers' ); ?></h2>
+		<h2 class="pev-card__title"><?php esc_html_e( 'Bulk Keys Generator', 'wc-serial-numbers' ); ?></h2>
 	</div>
 	<div class="pev-card__body">
 		<p>
-			<?php esc_html_e( 'Generate keys in bulk for a product. You can generate keys in bulk for a product using this tool.', 'wc-serial-numbers-pro', 'wc-serial-numbers' ); ?>
+			<?php esc_html_e( 'Generate keys in bulk for a product. You can generate keys in bulk for a product using this tool.', 'wc-serial-numbers' ); ?>
 			<br>
 			<?php // Translators: %1$s and %2$s are HTML tags. ?>
-			<?php printf( esc_html__( '%1$sNote%2$s: Product key source will be automatically change to "Preset" if it is not already set & Generated keys will be treated as preset keys.', 'wc-serial-numbers-pro', 'wc-serial-numbers' ), '<strong>', '</strong>' ); ?>
+			<?php printf( esc_html__( '%1$sNote%2$s: Product key source will be automatically change to "Preset" if it is not already set & Generated keys will be treated as preset keys.', 'wc-serial-numbers' ), '<strong>', '</strong>' ); ?>
 		</p>
 
 		<form method="POST" action="<?php echo esc_attr( admin_url( 'admin-post.php' ) ); ?>" name="wcsn-bulk-key-generator" id="wcsn-bulk-key-generator" class="wcsn-bulk-key-generator inline--fields">
 			<div class="pev-form-field">
-				<label for="product_id"><?php esc_html_e( 'Product *', 'wc-serial-numbers-pro', 'wc-serial-numbers' ); ?></label>
+				<label for="product_id"><?php esc_html_e( 'Product *', 'wc-serial-numbers' ); ?></label>
 				<select class="wcsn_search_product regular-text" name="product_id" id="product_id" required>
-					<option value=""><?php esc_html_e( 'All Products', 'wc-serial-numbers-pro', 'wc-serial-numbers' ); ?></option>
+					<option value=""><?php esc_html_e( 'All Products', 'wc-serial-numbers' ); ?></option>
 					<?php
 					$products = wc_get_products( array() );
 					foreach ( $products as $product ) :
@@ -34,14 +34,14 @@ defined( 'ABSPATH' ) || exit;
 					<?php endforeach; ?>
 				</select>
 				<p class="description">
-					<?php esc_html_e( 'Select the product associated with the key.', 'wc-serial-numbers-pro', 'wc-serial-numbers' ); ?>
+					<?php esc_html_e( 'Select the product associated with the key.', 'wc-serial-numbers' ); ?>
 				</p>
 			</div>
 
 			<div class="pev-form-field">
-				<label for="generator_id"><?php esc_html_e( 'Generator', 'wc-serial-numbers-pro', 'wc-serial-numbers' ); ?></label>
-				<select id="generator_id" name="generator_id" class="wcsn_select2 regular-text" data-action="wcsn_json_search" data-type="generator" data-nonce="<?php echo esc_attr( wp_create_nonce( 'wcsn_json_search' ) ); ?>" data-placeholder="<?php esc_attr_e( 'Select a  Generator...', 'wc-serial-numbers-pro', 'wc-serial-numbers' ); ?>">
-					<option value=""><?php esc_html_e( 'Select a key generator...', 'wc-serial-numbers-pro', 'wc-serial-numbers' ); ?></option>
+				<label for="generator_id"><?php esc_html_e( 'Generator', 'wc-serial-numbers' ); ?></label>
+				<select id="generator_id" name="generator_id" class="wcsn_select2 regular-text" data-action="wcsn_json_search" data-type="generator" data-nonce="<?php echo esc_attr( wp_create_nonce( 'wcsn_json_search' ) ); ?>" data-placeholder="<?php esc_attr_e( 'Select a  Generator...', 'wc-serial-numbers' ); ?>">
+					<option value=""><?php esc_html_e( 'Select a key generator...', 'wc-serial-numbers' ); ?></option>
 					<?php
 					$generators = wcsn_get_generators(
 						array(
@@ -54,17 +54,17 @@ defined( 'ABSPATH' ) || exit;
 					<?php endforeach; ?>
 				</select>
 				<p class="description">
-					<?php esc_html_e( 'Select a specific key generator or leave empty to use default settings.', 'wc-serial-numbers-pro', 'wc-serial-numbers' ); ?>
+					<?php esc_html_e( 'Select a specific key generator or leave empty to use default settings.', 'wc-serial-numbers' ); ?>
 				</p>
 
 			</div>
 
 			<!-- Quantity -->
 			<div class="pev-form-field">
-				<label for="quantity"><?php esc_html_e( 'Quantity *', 'wc-serial-numbers-pro', 'wc-serial-numbers' ); ?></label>
+				<label for="quantity"><?php esc_html_e( 'Quantity *', 'wc-serial-numbers' ); ?></label>
 				<input type="number" id="quantity" name="quantity"  class="regular-text" value="10" max="500" required>
 				<p class="description">
-					<?php esc_html_e( 'Enter the number of keys to generate.', 'wc-serial-numbers-pro', 'wc-serial-numbers' ); ?>
+					<?php esc_html_e( 'Enter the number of keys to generate.', 'wc-serial-numbers' ); ?>
 				</p>
 			</div>
 			<?php wp_nonce_field( 'wcsn_generate_bulk_keys' ); ?>
@@ -72,6 +72,6 @@ defined( 'ABSPATH' ) || exit;
 		</form>
 	</div>
 	<div class="pev-card__footer">
-		<button type="submit" form="wcsn-bulk-key-generator" id="wcsn-bulk-key-generator-btn" class="button button-primary"><?php esc_html_e( 'Generate Serial Keys', 'wc-serial-numbers-pro', 'wc-serial-numbers' ); ?></button>
+		<button type="submit" form="wcsn-bulk-key-generator" id="wcsn-bulk-key-generator-btn" class="button button-primary"><?php esc_html_e( 'Generate Serial Keys', 'wc-serial-numbers' ); ?></button>
 	</div>
 </div>
