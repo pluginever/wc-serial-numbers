c33cba227047b503ae608044b5b40dccddb3e8fc
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2025-10-21 17:52:00 +0600

    Add halloween offer

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
 		'js/admin-script': './assets/js/admin-script.js',
 		'js/frontend-script': './assets/js/frontend-script.js',
 		'css/admin-style': './assets/css/admin-style.scss',
+		'css/admin-halloween': './assets/css/halloween.scss',
 		'css/frontend-style': './assets/css/frontend-style.scss',
 	},
 	output: {
